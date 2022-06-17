e64dfc1ab58dd4cc85fa5c1a4efa0c3a6e2dab4d
Sidnal Arun (Consultant) <arun.sidnal@consultant.volvo.com>
2022-06-17 08:50:58 +0530

    changes

--- APIContainer/Program.cs
+++ APIContainer/Program.cs
@@ -15,6 +15,7 @@ namespace APIContainer
         {
             CreateHostBuilder(args).Build().Run();
         }
+        
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
